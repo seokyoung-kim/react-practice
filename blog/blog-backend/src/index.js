5c9da627bffb7d0332565472868f1e5d837f76cd
seokyoungkimm <seokyoungkimm@gmail.com>
2020-07-09 21:58:40 +0900

    라우트 모듈화

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -4,6 +4,8 @@ const Router = require('koa-router');
 const app = new Koa();
 const router = new Router();
 
+const api = require('./api');
+
 router.get('/', ctx => {
     ctx.body = '홈';
 })
@@ -17,6 +19,8 @@ router.get('/posts', ctx => {
     const {id} = ctx.query;
     ctx.body = id ? `포스트 #${id}` : '포스트 아이디가 없습니다.';
 })
+
+router.use('/api', api.routes());
 
 app.use(router.routes()).use(router.allowedMethods());
 // app.use(async (ctx, next) => {
