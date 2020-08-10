50fd171eef6f9f26bdd71021e078e5244614e19c
seokyoungkimm <seokyoungkimm@gmail.com>
2020-08-10 22:14:24 +0900

    스토어 생성

--- react-redux-tutorial/src/index.js
+++ react-redux-tutorial/src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import rootReducer from './modules';
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(
   <React.StrictMode>
