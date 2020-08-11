d89d85f26ef09515dbe36cb1f4a4f44fa07f35a9
seokyoungkimm <seokyoungkimm@gmail.com>
2020-08-11 21:13:55 +0900

    bindActionCreators 사용

--- react-redux-tutorial/src/containers/CounterContainer.js
+++ react-redux-tutorial/src/containers/CounterContainer.js
@@ -1,6 +1,8 @@
 import React from 'react'
+import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux'
 import Counter from '../components/Counter'
+import { increase, decrease } from '../modules/counter'
 
 const CounterContainer = ({number, increase, decrease}) => {
     return (
@@ -8,17 +10,16 @@ const CounterContainer = ({number, increase, decrease}) => {
     )
 }
 
-const mapStateToProps = state => ({
-    number: state.counter.number,
-})
-
-const mapDispatchToProps = dispatch => ({
-    increase: () => {
-        console.log('increase');
-    },
-    decrease: () => {
-        console.log('decrease');
-    }
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
+export default connect(
+    state => ({
+        number: state.counter.number,
+    }),
+    dispatch =>
+        bindActionCreators(
+            {
+                increase,
+                decrease,
+            },
+            dispatch
+        )
+)(CounterContainer);
