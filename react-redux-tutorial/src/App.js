caf6f1ab18576a8c5bb49fd26eece704cd266a3f
seokyoungkimm <seokyoungkimm@gmail.com>
2020-08-11 21:10:21 +0900

    Counter Container 작성

--- react-redux-tutorial/src/App.js
+++ react-redux-tutorial/src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './App.css';
-import Counter from './components/Counter';
 import Todos from './components/Todos';
+import CounterContainer from './containers/CounterContainer';
 
 function App() {
   return (
-    <>
-    <Counter number={0}/>
-    <hr />
-    <Todos />
-    </>
+    <div>
+      <CounterContainer />
+      <hr />
+      <Todos />
+    </div>
   );
 }
 
