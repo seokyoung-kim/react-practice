4ef0c941d363bba54438fdaee0f300256e139986
seokyoungkimm <seokyoungkimm@gmail.com>
2020-08-16 21:51:42 +0900

    Context, Consumer  사용

---
+++ context-tutorial/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ColorBox from './contexts/ColorBox';
+// import ColorContext from './contexts/color';
+
+function App() {
+  return (
+    <ColorBox />
+  );
+}
+
+export default App;
