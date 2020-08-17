71999d982ad1ebb4496e457e62bcb28e97dec373
seokyoungkimm <seokyoungkimm@gmail.com>
2020-08-17 23:09:41 +0900

    useContext 사용

--- context-tutorial/src/App.js
+++ context-tutorial/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ColorBox from './contexts/ColorBox';
-import ColorContext from './contexts/color';
+import { ColorProvider } from './contexts/color';
 
 function App() {
   return (
-    <ColorContext.Provider value={{color:'red'}} >
+    <ColorProvider>
       <div>
         <ColorBox />
       </div>
-    </ColorContext.Provider>
+    </ColorProvider>
   );
 }
 
