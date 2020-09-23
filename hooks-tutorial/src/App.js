2561c5828ba1603815432874c7df8fdb2cd4a743
seokyoungkimm <seokyoungkimm@gmail.com>
2020-09-23 20:52:20 +0900

    useInput

--- hooks-tutorial/src/App.js
+++ hooks-tutorial/src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
-import StyledComponent from './StyledComponent';
+import React, { useState } from "react";
+
+const useInput = inintialValue => {
+  const [value, setValue] = useState(inintialValue);
+  const onChange = event => {
+    console.log(event.tartget);
+  }
+  return { value, onChange };
+};
 
 const App = () => {
-  return(
-    <div>
-      <StyledComponent />
-    </div>
-  )
-}
+  const name = useInput("Mr.")
+
+  return (
+    <>
+      <h1>Hello</h1>
+      <input placeholder="Name" {...name} />
+    </>
+  );
+};
 export default App;
