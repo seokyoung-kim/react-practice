3f7e6ba912815c24cbc426be593a3121b46ed62f
seokyoungkimm <seokyoungkimm@gmail.com>
2020-12-05 21:23:33 +0900

    AuthTemplate

---
+++ blog/blog-frontend/src/components/auth/AuthTemplate.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const AuthTemplateBlock = styled.div``
+
+const AuthTemplate = () => {
+  return (
+    <AuthTemplateBlock>
+
+    </AuthTemplateBlock>
+  )
+}
+
+export default AuthTemplate;
